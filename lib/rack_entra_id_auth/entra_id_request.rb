d35978e2f3d1c1eb46342ecfb0a141e6aa368c5a
David Susco <dsusco@clarku.edu>
2024-07-24 09:44:17 -0400

    added middleware logic and EntraIdRequest helper object

---
+++ lib/rack_entra_id_auth/entra_id_request.rb
@@ -0,0 +1,201 @@
+require 'uri'
+
+module RackEntraIdAuth
+  class EntraIdRequest
+    attr_reader :request
+
+    def initialize(request, saml_setting_overrides = {})
+      @request = request
+
+      @saml_settings = OneLogin::RubySaml::Settings.new(RackEntraIdAuth.config.ruby_saml_settings.merge(saml_setting_overrides))
+    end
+
+    # Returns the request's base URL and path without the path_info at the end.
+    #
+    # @return [String]
+    #
+    def base_url
+      "#{request.base_url}#{request.path}".sub(Regexp.new("#{request.path_info}$"), '')
+    end
+
+    # Returns whether the request is a Service Provider initiated sign-on request.
+    # Returns true if the request's path info equals the login path configuration (login_path), otherwise returns false.
+    #
+    # @return [Bool]
+    #
+    def login?
+      request.path_info.eql?(RackEntraIdAuth.config.login_path)
+    end
+
+    # Returns whether the request contains a single sign-on response (for Service Provider initiated single sign-on requests).
+    # Returns true if the request's header contains a SAMLResponse and if the request's base_url and path match the ACS service url setting (assertion_consumer_service_url), otherwise returns false.
+    #
+    # @return [Bool]
+    #
+    def login_response?
+      saml_response.present? and "#{request.base_url}#{request.path}".eql?(@saml_settings.assertion_consumer_service_url)
+    end
+
+    # Returns whether the request is a Service Provider initiated logout request.
+    # Returns true if the request's path info equals the logout path configuration (logout_path), otherwise returns false.
+    #
+    # @return [Bool]
+    #
+    def logout?
+      request.path_info.eql?(RackEntraIdAuth.config.logout_path)
+    end
+
+    # Returns whether the request contains a single logout request (for ID Provider initiated single logout requests).
+    # Returns true if the request contains a SAMLRequest query parameter and if the request's base_url and path match the single logout service url setting (single_logout_service_url), otherwise returns false.
+    #
+    # @return [Bool]
+    #
+    def logout_request?
+      request.params['SAMLRequest'].present? and "#{request.base_url}#{request.path}".eql?(@saml_settings.single_logout_service_url)
+    end
+
+    # Returns whether the request contains a single logout response for Service Provider initiated logout request.
+    # Returns true if the request contains a SAMLResponse query parameter and if the request's base_url and path match the single logout service url setting (single_logout_service_url), otherwise returns false.
+    #
+    # @return [Bool]
+    #
+    def logout_response?
+      request.params['SAMLResponse'].present? and "#{request.base_url}#{request.path}".eql?(@saml_settings.single_logout_service_url)
+    end
+
+    # Returns the RelayState in the header of the request.
+    #
+    # @return [String]
+    #
+    def relay_state_url
+      request.get_header('rack.request.form_hash')['RelayState']
+    end
+
+    # A single sign-on response for the SAMLResponse in the request's header.
+    # This is the response sent by the ID Provider for Service Provider initiated single sign-on requests.
+    #
+    # @param auth_request_id [String] If provided, check that the inResponseTo in the response matches the uuid of the sign-on request that initiated the response.
+    # @param skip_conditions [Bool] Skip the conditions validation.
+    # @param allowed_clock_drift [Float] The allowed clock drift when checking time stamps.
+    # @param skip_subject_confirmation [Bool] Skip the subject confirmation validation.
+    # @param skip_recipient_check [Bool] Skip the recipient validation of the subject confirmation element.
+    # @param skip_audience [Bool] Skip the audience validation.
+    #
+    # @return [OneLogin::RubySaml::Response] A single sign-on response for a Service Provideer initiated single sign-on request.
+    #
+    def saml_auth_response (auth_request_id: request.session[:auth_request_id], skip_conditions: false, allowed_clock_drift: nil, skip_subject_confirmation: false, skip_recipient_check: false, skip_audience: false)
+      response = OneLogin::RubySaml::Response.new(
+        saml_response,
+        { :settings => @saml_settings,
+          :matches_request_id => auth_request_id,
+          :skip_conditions => skip_conditions,
+          :allowed_clock_drift => allowed_clock_drift,
+          :skip_subject_confirmation => skip_subject_confirmation,
+          :skip_recipient_check => skip_recipient_check,
+          :skip_audience => skip_audience })
+
+      # the auth request's ID is no longer needed
+      request.session.delete(:auth_request_id)
+
+      response
+    end
+
+    # A single logout request for the SAMLRequest in the request's query parameters.
+    # This is the request sent by the ID Provider for ID Provider initiated single logout requests.
+    #
+    # @param allowed_clock_drift [Float] The allowed clock drift when checking time stamps.
+    # @param relax_signature_validation [Bool] If true and there's no ID Provider certs in the settings then ignore the signature validation on the request.
+    #
+    # @return [OneLogin::RubySaml::Logoutresponse] A single logout response for a Service Provideer initiated single logout request.
+    #
+    def saml_logout_request (allowed_clock_drift: nil, relax_signature_validation: false)
+      OneLogin::RubySaml::SloLogoutrequest.new(
+        request.params['SAMLRequest'],
+        { :settings => @saml_settings,
+          :allowed_clock_drift => allowed_clock_drift,
+          :relax_signature_validation => relax_signature_validation })
+    end
+
+    # A single logout response for the SAMLResponse in the request's query parameters.
+    # This is the response sent by the ID Provider for Service Provider initiated single logout requests.
+    #
+    # @param logout_request_id [String] If provided, check that the inResponseTo in the response matches the uuid of the logout request that initiated the response.
+    # @param relax_signature_validation [Bool] If true and there's no ID Provider certs in the settings then ignore the signature validation on the response.
+    #
+    # @return [OneLogin::RubySaml::Logoutresponse] A single logout response for a Service Provideer initiated single logout request.
+    #
+    def saml_logout_response (logout_request_id: request.session[:logout_request_id], relax_signature_validation: false)
+      logout_response = OneLogin::RubySaml::Logoutresponse.new(
+        request.params['SAMLResponse'],
+        @saml_settings,
+        { :get_params => request.params,
+          :matches_request_id => logout_request_id,
+          :relax_signature_validation => relax_signature_validation })
+
+      # the logout request's ID is no longer needed
+      request.session.delete(:logout_request_id)
+
+      logout_response
+    end
+
+    # Returns a single logout reponse URL for the settings provided.
+    # Used for ID Provider initiated log outs.
+    #
+    # @param request_id [String] The ID of the LogoutRequest sent by this SP to the IdP. That ID will be placed as the InResponseTo in the logout response.
+    # @param logout_message [String] The message to be placed as StatusMessage in the logout response.
+    # @param params [Hash] Extra query parameters to be added to the URL (e.g. RelayState).
+    # @param logout_status_code [String] The StatusCode to be placed as StatusMessage in the logout response.
+    #
+    # @return [String]
+    #
+    def slo_response_url (request_id: nil, logout_message: nil, params: {}, logout_status_code: nil)
+      OneLogin::RubySaml::SloLogoutresponse.new.create(
+        @saml_settings,
+        request_id,
+        logout_message,
+        params,
+        logout_status_code)
+    end
+
+    # Returns a single logout request URL for the settings provided if an ID Provider single logout target URL is present in the settings (idp_slo_service_url), otherwise returns nil.
+    # Used for Service Provider initiated log outs.
+    #
+    # @param params [Hash] Extra query parameters to be added to the URL (e.g. RelayState).
+    #
+    # @return [String|nil]
+    #
+    def slo_url (params = {})
+      logout_request = OneLogin::RubySaml::Logoutrequest.new
+
+      if @saml_settings.idp_slo_service_url.present?
+        # store the logout request's uuid to validate it in the response
+        request.session[:logout_request_id] = logout_request.uuid
+
+        # return nil if no single logout url is set
+        logout_request.create(@saml_settings, params)
+      end
+    end
+
+    # Returns a single sign-on authentication request URL for the settings provided.
+    # Used for Service Provider initiated sign-ins.
+    #
+    # @param params [Hash] Extra query parameters to be added to the URL (e.g. RelayState).
+    #
+    # @return [String]
+    #
+    def sso_url (params = {})
+      auth_request = OneLogin::RubySaml::Authrequest.new
+
+      # store the auth request's uuid to validate it in the response
+      request.session[:auth_request_id] = auth_request.uuid
+
+      auth_request.create(@saml_settings, params)
+    end
+
+    private
+
+    def saml_response
+      request.get_header('rack.request.form_hash').try(:[], 'SAMLResponse')
+    end
+  end
+end
--- lib/rack_entra_id_auth/middleware.rb
+++ lib/rack_entra_id_auth/middleware.rb
@@ -1,11 +1,183 @@
+require 'rack_entra_id_auth/entra_id_request'
+
 module RackEntraIdAuth
   class Middleware
-    def initialize (app, config = {})
+    def initialize (app)
       @app = app
-      @config = RackEntraIdAuth.config.ruby_saml_settings.merge(config)
     end
 
     def call (env)
+      request = Rack::Request.new(env)
+      entra_id_request = EntraIdRequest.new(request)
+
+      # SP initiated single sign-on request
+      if entra_id_request.login?
+        log(env, 'Redirecting login request to Entra ID single sign-on URL…')
+
+        sso_url = entra_id_request.sso_url(
+          { :RelayState => request.params['relay_state'] || RackEntraIdAuth.config.login_relay_state_url || entra_id_request.base_url })
+
+        return found_redirect_response(
+          sso_url,
+          'Redirecting login request to Entra ID single sign-on URL')
+      end
+
+      # SP initiated logout/single logout request
+      if entra_id_request.logout?
+        log(env, 'Destroying session…')
+
+        # destroy session in case single logout fails
+        request.session.send (request.session.respond_to?(:destroy) ? :destroy : :clear)
+
+        relay_state_url = request.params['relay_state'] || RackEntraIdAuth.config.logout_relay_state_url || entra_id_request.base_url
+
+        slo_url = entra_id_request.slo_url({ :RelayState => relay_state_url })
+
+        if request.params['skip_single_logout'].blank? and
+           !RackEntraIdAuth.config.skip_single_logout and
+           slo_url.present?
+
+          log(env, 'Redirecting logout request to Entra ID single logout URL…')
+
+          return found_redirect_response(
+            slo_url,
+            'Redirecting logout request to Entra ID single logout URL')
+        end
+
+        log(env, 'Skipping single logout because of skip_single_logout query parameter…') if request.params['skip_single_logout'].present?
+        log(env, 'Skipping single logout because of skip_single_logout configuration setting…') if RackEntraIdAuth.config.skip_single_logout
+        log(env, 'Skipping single logout because no Entra ID single logout URL was found…') if slo_url.blank?
+
+        log(env, 'Redirecting to relay state URL…')
+
+        return found_redirect_response(relay_state_url)
+      end
+
+      # SP initiatied single sign-on response
+      if entra_id_request.login_response?
+        log(env, "Received single login response…")
+
+        auth_response = entra_id_request.saml_auth_response()
+
+        if !auth_response.is_valid?
+          log(env, "Invalid single login reponse from Entra ID: #{auth_response.errors.first}")
+
+          return internal_server_error_response("Invalid login reponse from Entra ID: #{auth_response.errors.first}")
+        end
+
+        if !auth_response.success?
+          log(env, 'Unsuccessful single single reponse from Entra ID.')
+
+          return internal_server_error_response('Unsuccessful login reponse from Entra ID.')
+        end
+
+        log(env, "Initializing session and redirecting to relay state URL…")
+
+        # initialize the session with the response's SAML attributes
+        request.session[RackEntraIdAuth.config.session_key] = RackEntraIdAuth.config.session_value_proc.call(auth_response)
+
+        return found_redirect_response(
+                 entra_id_request.relay_state_url || RackEntraIdAuth.config.login_relay_state_url || entra_id_request.base_url,
+                 'Received single login response, redirecting to relay state URL')
+      end
+
+      # IdP initiatied single logout request
+      if entra_id_request.logout_request?
+        log(env, "Received single logout request…")
+
+        logout_request = entra_id_request.saml_logout_request()
+
+        if !logout_request.is_valid?
+          log(env, "Invalid single logout request from Entra ID: #{logout_request.errors.first}")
+
+          return internal_server_error_response("Invalid logout request from Entra ID: #{logout_request.errors.first}")
+        end
+
+        log(env, "Destroying session and sending logout response to Entra ID…")
+
+        # destroy the session
+        request.session.send (request.session.respond_to?(:destroy) ? :destroy : :clear)
+
+        response_url = entra_id_request.slo_response_url(
+          request_id: logout_request.id,
+          logout_message: nil,
+          params: {
+            :RelayState => entra_id_request.relay_state_url || RackEntraIdAuth.config.logout_relay_state_url || entra_id_request.base_url
+          },
+          logout_status_code: nil)
+
+        return found_redirect_response(
+                 response_url,
+                 'Received single logout request, redirecting to Entra ID')
+      end
+
+      # SP initiated single logout response
+      if entra_id_request.logout_response?
+        log(env, "Received single logout response…")
+
+        logout_response = entra_id_request.saml_logout_response()
+
+        if !logout_response.validate
+          log(env, "Invalid single logout reponse from Entra ID: #{logout_response.errors.first}")
+
+          return internal_server_error_response("Invalid logout reponse from Entra ID: #{logout_response.errors.first}")
+        end
+
+        if !logout_response.success?
+          log(env, "Unsuccessful single logout reponse from Entra ID.")
+
+          return internal_server_error_response('Unsuccessful logout reponse from Entra ID.')
+        end
+
+        log(env, "Destroying session and redirecting to relay state URL…")
+
+        # session should already be destroyed from SP initiated logout/single logout request, but just to be safe…
+        request.session.send (request.session.respond_to?(:destroy) ? :destroy : :clear)
+
+        return found_redirect_response(
+                 entra_id_request.relay_state_url || RackEntraIdAuth.config.logout_relay_state_url || entra_id_request.base_url,
+                 'Received single logout response, redirecting to relay state URL')
+      end
+
+      response = @app.call(env)
+
+      # Authenticate 401s
+      if response[0] == 401
+        log(env, 'Intercepted 401 Unauthorized response, redirecting to Entra ID single sign-on URL…')
+
+        return found_redirect_response(
+                 entra_id_request.sso_url(:RelayState => request.url),
+                 'Intercepted 401 Unauthorized response, redirecting to Entra ID single sign-on URL')
+      end
+
+      response
     end
+
+    protected
+
+      def found_redirect_response (url, message = "Redirecting to URL")
+        [ 302,
+          { 'location' => url,
+            'content-type' => 'text/plain' },
+          [ "#{message}: #{url}" ] ]
+      end
+
+      def internal_server_error_response (content = "Internal server error")
+        [ 500,
+          { 'content-type' => 'text/html',
+            'content-length' => content.length},
+            [ content] ]
+      end
+
+      def log(env, message, level = :info)
+        env['rack.logger'] ||= Rails.logger if defined?(Rails.logger)
+        message = "rack-entra-id-auth: #{message}"
+
+        if env['rack.logger']
+          env['rack.logger'].send(level, message)
+        else
+          env['rack.errors'].write(message.concat("\n"))
+        end
+      end
   end
 end
